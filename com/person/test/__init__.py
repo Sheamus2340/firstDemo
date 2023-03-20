fc241d458465fabb8bec8bfb33e1bed2d797bd27
Sheamus <377890470@qq.com>
2023-03-21 01:23:21 +0800

    Initial commit

---
+++ com/person/test/__init__.py
@@ -0,0 +1,16 @@
+"""
+特殊注释：
+首行注释：只能在首行和shell脚本一样
+1. python解析器路径；
+2. 编码格式
+例子：
+#/usr/bin/python3
+#-- coding=utf-8 --
+
+"""
+
+# 单行注释
+print("hello world!!")
+'''
+多行注释
+'''
---
+++ com/person/test/index.py
@@ -0,0 +1,44 @@
+# 数据类型、命名规则、命名规范
+a = 10
+print(a)
+a = 1 + 1
+
+# int 类型
+print(a)
+print(type(a))
+
+# bool 类型
+a = True
+print(type(a))
+
+# 字符串类型
+b = "b"
+print(type(b))
+
+# 元组类型
+c = ()
+print(type(c))
+
+# 列表类型
+d = []
+print(type(d))
+
+# 字典类型
+e = {}
+print(type(e))
+
+# 变量命名规则
+"""
+1. 变量必须以字母和下划线开头；
+2. 其他可以是字母、数字、下划线；
+3. 变量区分大小写；
+4. python关键字作为变量名；
+"""
+
+# 命名规范
+"""
+1. 见名识意；
+2. 小驼峰命名：userName
+3. 大驼峰命名：UserName
+4. 下划线命名：user_name
+"""
