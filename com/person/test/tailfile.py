d85fd729c6d0fb008ae3def558fd3604dbd1d752
Sheamus <377890470@qq.com>
2023-05-03 22:38:02 +0800

    添加修改

--- com/person/test/tailfile.py
+++ com/person/test/tailfile.py
@@ -1,8 +1,27 @@
 # coding: utf-8
 # @Author: Sheamus
 
-with open('data/user.log', mode='a+t', encoding='utf-8') as file:
+# with open('data/user.log', mode='a+t', encoding='utf-8') as file:
+#     while 1:
+#         res = file.readline()
+#         if res:
+#             print(res.strip())
+
+# import time
+# with open('data/user.log', mode='rt', encoding='utf-8') as file:
+#     file.seek(0, 2)
+#     while 1:
+#         res = file.readline()
+#         if res:
+#             print(res.strip())
+#         time.sleep(0.2)
+
+import time
+
+with open('data/user.log', mode='rb') as file:
+    file.seek(0, 2)
     while 1:
         res = file.readline()
         if res:
-            print(res.strip())
+            print(res.strip().decode('utf-8'))
+        time.sleep(0.2)
