c5b425ed7be0e0560e5fa423c32d4150d790860e
Sheamus <377890470@qq.com>
2023-05-03 22:09:33 +0800

    python的基础代码

--- com/person/test/tailfile.py
+++ com/person/test/tailfile.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 # @Author: Sheamus
 
-with open('data/user.log', mode='rt', encoding='utf-8') as file:
+with open('data/user.log', mode='a+t', encoding='utf-8') as file:
     while 1:
         res = file.readline()
         if res:
