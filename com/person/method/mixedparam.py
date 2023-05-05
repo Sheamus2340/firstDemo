5f3462d1fca5138587a38147217acc31b63bbc3a
Sheamus <377890470@qq.com>
2023-05-05 23:26:11 +0800

    函数的定义和调用

--- com/person/method/mixedparam.py
+++ com/person/method/mixedparam.py
@@ -5,9 +5,25 @@ def func(x, y, z):
 
 
 def func2(*args, **kwargs):
+    print(f'打印形参的数据是什么: {args} ,{kwargs}')  # 打印形参的数据是什么
     func(*args, **kwargs)
 
 
 func2(1, 2, 3)
 
 func2(1, 2, z='value')
+
+
+def func3(**kwargs):
+    print(kwargs)
+
+
+dict_test = {'a': 1}
+func3(**dict_test)
+
+
+def func4(a):
+    print(a)
+
+
+func4(**dict_test)
